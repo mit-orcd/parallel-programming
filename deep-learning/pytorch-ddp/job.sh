7f5778f73dfb970ba4b21d4f4e78cf5efb8575f9
shaohao99 <shaohao@eofe10.mit.edu>
2025-01-13 19:55:10 -0500

    update

--- deep-learning/pytorch-ddp/job.sh
+++ deep-learning/pytorch-ddp/job.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=ddp
 #SBATCH -N 1
 #SBATCH -n 2
-#SBATCH --gres=gpu:h100:2
+#SBATCH --gres=gpu:2
 #SBATCH -o output/%N-%J.out
 
 module load miniforge/23.11.0-0
@@ -12,6 +12,6 @@ source activate ds
 #usage: multigpu.py [-h] [--batch_size BATCH_SIZE] total_epochs save_every
 echo "======== Run on one GPU ======"
 python single_gpu.py --batch_size=1024 100 20
-echo "======== Run on one multiple GPUs ======"
+echo "======== Run on multiple GPUs ======"
 python multigpu.py --batch_size=1024 100 20
 
